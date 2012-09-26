8b1d501eff6d3a2ac3cc334085ba14898037592b
emily.castles <emily.castles@redhills.ie>
2012-09-26 17:20:44 +0100

    intial commit - refinery set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emilycastles::Application.config.secret_token = 'c7236cbdd059c8c956a6856b57d4b96c04cb00e7ec04ec3d7ab2dd932e8e3e66d6546e2e3535b566b4f8349cef758aaa7cf65608f9bbcc5b7d59887772417579'
